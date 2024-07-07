54a54c95fc5bf32bb0ad576ab1069e798fdbc5bd
anuragchaturvedi <anuragchaturvedi@localhost>
2024-07-07 20:22:56 +0530

    code update for schema Validator and response Specification

--- src/test/java/crudOperaion/ReqSpec_ResSpec.java
+++ src/test/java/crudOperaion/ReqSpec_ResSpec.java
@@ -9,8 +9,11 @@ import static org.testng.Assert.assertEquals;
 
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -19,26 +22,55 @@ import io.restassured.specification.ResponseSpecification;
 public class ReqSpec_ResSpec {
 	static Response response;
 	RequestSpecification reqSpec;
-	ResponseSpecification resSpec;
+	static ResponseSpecification resSpec;
+	
+	
+	@BeforeClass
+	public void setupResSpec() {	
+		// Create a ResponseSpecification 
+				resSpec=  RestAssured.expect();
+				resSpec.contentType(ContentType.JSON);
+				resSpec.statusCode(200);
+				resSpec.time(Matchers.lessThan(5000L));
+				System.out.println("matches response expected time ");
+				resSpec.statusLine("HTTP/1.1 200 OK");
+				System.out.println("matches response status line & code ");
+				System.out.println("Before class Response spec matches successfully");
+
+	}
 
 	@Test
 	public void test_3() {
 		baseURI = "https://reqres.in/api/";
 
-		reqSpec = given().header("Content-Type", "application/json").contentType(ContentType.JSON)
+		reqSpec = given()
+				.header("Content-Type", "application/json")
+				.contentType(ContentType.JSON)
 				.accept(ContentType.JSON);
 		response = reqSpec.when().get("users?page=2");
 		int status = response.statusCode();
+		response.then().spec(resSpec);
 		response = response.then().extract().response();
 		assertEquals(status, 200);
 		System.out.println(response.getBody().jsonPath().getString("data.id"));
-		response.then().body("data.id", hasItems(11, 12));
-		response.then().log().body(DEFAULT_URL_ENCODING_ENABLED);
+		response.then()
+		.body("data.id", hasItems(11, 12));
+		response.then()
+		.log().body(DEFAULT_URL_ENCODING_ENABLED);
+		
+		// Initialize ResponseSpecification
+        resSpec = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .expectResponseTime(Matchers.lessThan(5000L))
+                .build();
+		System.out.println("Within test maethod Response spec matches successfully");
 
-		resSpec = resSpec.expect();
-		resSpec.statusCode(200);
-		resSpec.contentType(ContentType.JSON);
-		resSpec.time(Matchers.lessThan(5000L));
-
+        // Apply ResponseSpecification to validate the response
+        response.then().spec(resSpec);
+		
+        
+       
+		
 	}
 }
