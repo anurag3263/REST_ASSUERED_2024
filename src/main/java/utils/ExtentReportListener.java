7e5187202f0dc1b5d99cd00f86c358421220342a
anuragchaturvedi <anuragchaturvedi@localhost>
2024-07-11 23:48:27 +0530

    updated extent report configuration

--- src/main/java/utils/ExtentReportListener.java
+++ src/main/java/utils/ExtentReportListener.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.io.IOException;
+
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -12,45 +14,52 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 public class ExtentReportListener implements ITestListener {
 
 	private ExtentReports extent;
-	private ExtentTest test;
+    private ExtentTest test;
 
-	@Override
-	public void onStart(ITestContext context) {
-		ExtentSparkReporter htmlReporter = new ExtentSparkReporter("target/extent.html");
-		extent = new ExtentReports();
-		extent.attachReporter(htmlReporter);
-	}
+    @Override
+    public void onStart(ITestContext context) {
+        ExtentSparkReporter htmlReporter = new ExtentSparkReporter("target/extentReport.html");
+        
+        // Load configuration from extent-config.xml
+        try {
+			htmlReporter.loadXMLConfig("extent-config.xml");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+    }
 
-	@Override
-	public void onTestStart(ITestResult result) {
-		test = extent.createTest(result.getMethod().getMethodName());
-	}
+    @Override
+    public void onTestStart(ITestResult result) {
+        test = extent.createTest(result.getMethod().getMethodName());
+    }
 
-	@Override
-	public void onTestSuccess(ITestResult result) {
-		test.log(Status.PASS, "Test Case Passed: " + result.getName());
-	}
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.log(Status.PASS, "Test Case Passed: " + result.getName());
+    }
 
-	@Override
-	public void onTestFailure(ITestResult result) {
-		test.log(Status.FAIL, "Test Case Failed: " + result.getName());
-		test.log(Status.FAIL, "Failure Reason: " + result.getThrowable());
-	}
+    @Override
+    public void onTestFailure(ITestResult result) {
+        test.log(Status.FAIL, "Test Case Failed: " + result.getName());
+        test.log(Status.FAIL, "Failure Reason: " + result.getThrowable());
+    }
 
-	@Override
-	public void onFinish(ITestContext context) {
-		extent.flush();
-	}
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
 
-	@Override
-	public void onTestSkipped(ITestResult result) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        // Handle skipped tests if needed
+    }
 
-	@Override
-	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        // Handle tests that fail within success percentage if needed
+    }
 }
