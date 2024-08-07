b50daf49ca3e7b40e17d669373f0d4f938111d5d
anuragchaturvedi <anuragchaturvedi@localhost>
2024-08-07 23:37:44 +0530

    dynamic response templating

--- src/test/java/crudOperaion/WiremockTest.java
+++ src/test/java/crudOperaion/WiremockTest.java
@@ -1,5 +1,7 @@
 package crudOperaion;
 
+import static io.restassured.RestAssured.given;
+
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -8,14 +10,16 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import static io.restassured.RestAssured.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
 import io.restassured.response.Response;
 
 public class WiremockTest {
-
 	private static final String Host = "localhost";
 	private static final int Port = 8089;
 	private static WireMockServer server = new WireMockServer(Port);
 	private static final String END_POINT = "/readfromfile/index";
+	private static final String END_POINT2 = "/contract/{contractid}";
 	static Response response;
 
 	@BeforeClass
@@ -24,11 +28,21 @@ public class WiremockTest {
 
 		server.start();
 		WireMock.configureFor(Host, Port);
+		configureFor(Host, Port);
 
+		// Stub for /readfromfile/index
 		ResponseDefinitionBuilder mockResponse = new ResponseDefinitionBuilder();
 		mockResponse.withStatus(200);
 		mockResponse.withBodyFile("WiremockTestData/reqUsers2.json");
 		WireMock.stubFor(WireMock.get(END_POINT).willReturn(mockResponse));
+		stubFor(get(END_POINT).willReturn(mockResponse));
+
+		// Stub for /contract/{contractid} using the body file with templating
+		stubFor(get(urlPathTemplate("/contract/{contractid}"))
+		    .willReturn(aResponse()
+		        .withStatus(200)
+		        .withBodyFile("WiremockTestData/dynamicResTemp.json")
+		        .withTransformers("response-template")));
 	}
 
 	@Test
@@ -40,6 +54,14 @@ public class WiremockTest {
 
 	}
 
+	@Test
+	public void dynamicResponseTest() {
+		String testApi = "http://localhost:" + Port + END_POINT2;
+		System.out.println("Service to be hit ... " + testApi);
+		response = given().pathParam("contractid", "abc123").get(testApi).then().statusCode(200).extract().response();
+		System.out.println(response.asString());
+	}
+
 	@AfterClass
 	public void shutDownServer() {
 		if (server.isRunning() && null != server) {
@@ -47,5 +69,4 @@ public class WiremockTest {
 			server.shutdown();
 		}
 	}
-
 }
